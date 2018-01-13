c743c7837e7d009a4aa7bf847806d82a180947c2
Tomasz Mloduchowski <q@qdot.me>
2018-01-13 16:38:43 -0600

    Gifted Chat Example Slowdown

--- App.js
+++ App.js
@@ -12,6 +12,10 @@ import {
   View
 } from 'react-native';
 
+import {
+  GiftedChat,
+} from 'react-native-gifted-chat';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -20,38 +24,17 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+  state = {
+    messages: [],
+  };  
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+        <GiftedChat 
+          messages={this.state.messages}
+          onSend={(messages) => 0}
+          user = {{ _id: 1 }}
+        />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
